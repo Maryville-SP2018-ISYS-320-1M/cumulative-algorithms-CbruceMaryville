76abe099413d9d780e0027bc5768a131bf559561
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 22:50:41 -0500

    Update P8_ThresholdCounter.java

--- src/P8_ThresholdCounter.java
+++ src/P8_ThresholdCounter.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018 
 */
 
 public class P8_ThresholdCounter {
 
 	public static void main(String[] args) {
-
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("How many numbers are in the series? : ");
+		int series = console.nextInt();
+		System.out.print("What is your number threshold : ");
+		int thresh = console.nextInt();
+		int count = 0;
+		for(int i =0; i < series; i ++) {
+			
+			System.out.println("Please type number : " + (i+1));
+				if(console.nextInt() > thresh) {
+					count ++;
+				}
+		}
+		System.out.println("The count of numbers above the threshold is : " + count);
 	}
 
 }
