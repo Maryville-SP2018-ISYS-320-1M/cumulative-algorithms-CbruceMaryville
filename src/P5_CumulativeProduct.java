8daf02aa86f4bade1e24735a45bf32feb2a34c96
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 22:34:12 -0500

    Update P5_CumulativeProduct.java

--- src/P5_CumulativeProduct.java
+++ src/P5_CumulativeProduct.java
@@ -1,13 +1,34 @@
+//AM COMPLETELY STUCK ON THIS ONE BRAIN IS FRIED
+//Tried using an array but my brain says im fried =
+import java.util.Scanner;
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018 
 */
 
 public class P5_CumulativeProduct {
 
 	public static void main(String[] args) {
-
+		Scanner console = new Scanner(System.in);
+		
+		System.out.print("How many numbers do you want to multiply? : ");
+		int multi = console.nextInt();
+		
+		int[] finalprod = new int[50];
+		for(int i =0; i < multi; i ++) {
+			System.out.println("Enter number :" + i);
+			int nums;
+			nums = console.nextInt();
+			finalprod[i] = nums;
+		}
+		int endingnum =1;
+		for(int i = 0 i<finalprod.len(); i ++ {
+			System.out.println(finalprod[i]);
+			
+			//endingnum *= finalprod[i];
+			//System.out.println("Final product is: " + endingnum);
+		}
 		
 	}
 
