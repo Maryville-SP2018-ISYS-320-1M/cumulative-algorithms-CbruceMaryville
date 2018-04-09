c8a279e112a179c5d8a83eba4226ad8e70e6aa6a
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-08 21:56:41 -0500

    Update P1_FollowTheCumulativeLoop.java

--- src/P1_FollowTheCumulativeLoop.java
+++ src/P1_FollowTheCumulativeLoop.java
@@ -2,22 +2,30 @@ import java.util.Scanner;
 
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/8/2018
 */
 
 /*
 1. Fill out the table for the loop below for the user inputs:
 4 3 5 2 6
 
-	| ITERATION 		 | VARIABLE NAME	 | VARIABLE NAME	 | VARIABLE NAME	 |
-	| initialization	 |				 |				 |				 | 
-	| 1				 |				 |				 |				 |
+	
+	enter 4:
+	enter 3: 3 is < 4 so AV is now 3
+	enter 5: 3<5 so AV stays 3
+	enter 2: 2<3 SO AV is now 2
+	enter 6: 2<6 so AV stays 2 
+	final AV value of 2
+	
+	
+	
 */
 
 /*
 2. What is does this algorithm accomplish? (i.e., what does it compute or find?)
-
+ It keeps the number that is smallest. When you enter a smaller number then that will be the new
+ accumulated value.
 
 */
 
