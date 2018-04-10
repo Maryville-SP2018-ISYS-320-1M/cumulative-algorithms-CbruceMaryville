77fa6cdb2dbdccc0a047028e3341c28b0204984d
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-09 19:34:38 -0500

    Update P6_DoubleProblems.java

--- src/P6_DoubleProblems.java
+++ src/P6_DoubleProblems.java
@@ -1,4 +1,4 @@
-//PART 6-7 ALSO COMPLETELY STUCK??!!?!?!
+//PART 6-7 FIXED
 /*
 ISYS 320
 	Name(s):Clayton Bruce
@@ -9,14 +9,18 @@ public class P6_DoubleProblems {
 
 	public static void main(String[] args) {
 		double gpa = 3.2;
-		if( gpa * 3 == 9.6 ) {
-			System.out.println( "You earned enough credts");
+		if (Math.abs(gpa * 3 - 9.6) < 0.1) {
+			System.out.println("You earned enough credts");
+		} else {													// added this else for testing
+			System.out.println("The condition isn't met try again");
 		}
 	}
 
 }
 
 /*
- * It isn't executing d
+ * It isn't executing because: there is a roundoff error of about .000000001
+ * By using absolute value and checking for a roundoff error we are able to verify the error and work
+ * around it and meet our condition!
  * 
- * */
+ */
