10f75a7f014348a4f1ff366497087351029eb247
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-09 19:44:03 -0500

    Update P5_CumulativeProduct.java

--- src/P5_CumulativeProduct.java
+++ src/P5_CumulativeProduct.java
@@ -1,5 +1,5 @@
-//AM COMPLETELY STUCK ON THIS ONE BRAIN IS FRIED
-//Tried using an array but my brain says im fried =
+
+//Fixed in Class is working now!!
 import java.util.Scanner;
 /*
 	ISYS 320
@@ -11,25 +11,21 @@ public class P5_CumulativeProduct {
 
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
+		System.out.print("How many numbers do you want to enter? ");
+		int count = console.nextInt();
 		
-		System.out.print("How many numbers do you want to multiply? : ");
-		int multi = console.nextInt();
+		int product = 1;
 		
-		int[] finalprod = new int[50];
-		for(int i =0; i < multi; i ++) {
-			System.out.println("Enter number :" + i);
-			int nums;
-			nums = console.nextInt();
-			finalprod[i] = nums;
-		}
-		int endingnum =1;
-		for(int i = 0 i<finalprod.len(); i ++ {
-			System.out.println(finalprod[i]);
-			
-			//endingnum *= finalprod[i];
-			//System.out.println("Final product is: " + endingnum);
+		for (int i = 1; i <= count; i++) {
+		System.out.print("Enter the next number : " + i + " ");
+		int num = console.nextInt();
+		product *= num;
 		}
 		
-	}
-
+		
+		System.out.println("Product = " + product); 
+		}
 }
+
+
+
